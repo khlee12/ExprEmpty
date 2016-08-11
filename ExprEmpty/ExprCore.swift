35a0bea7ccdd5bb6859535fa37e4fc808b1ad5a1
khlee12 <khlee1290@gmail.com>
2016-08-11 12:25:28 +0900

    Update ExprCore.swift

--- ExprEmpty/ExprCore.swift
+++ ExprEmpty/ExprCore.swift
@@ -20,14 +20,75 @@ class ExprCore{
     func doIsPalindrome()->String{
         //check whether the operand is palindrome
         //if yes -> return "Yes", otherwise return "NO"
-        return "YES"
+        if self.operand.hasPrefix("+") || self.operand.hasPrefix("-")
+        {
+            return "NO"
+        }
+        let compere: Bool? = (self.operand == doReverse())
+        if compere! == true {
+            return "YES"
+        }
+        return "NO"
     }
+    
     func doReverse()->String{
         //reverse the operand(except the symbol) and return reversed string
-        return "reversed"
+        //let reversed = Array(self.operand.characters)[0]
+        let length = self.operand.characters.count
+        var reversed = Array(self.operand.characters)
+        var last = length - 1
+        var temp:Character
+        var start = 0
+        
+        if self.operand.hasPrefix("+") || self.operand.hasPrefix("-"){
+            start = 1
+        }
+        for i in start..<length {
+            if i < last{
+                temp = reversed[i]
+                reversed[i] = reversed[last]
+                reversed[last] = temp
+                last -= 1
+            }
+        }
+        
+        var result:String = ""
+        
+        for i in 0..<length {
+            result.append(reversed[i])
+        }
+        return result
     }
     func doStrStr(needles:String)->Int{
+        //operand -> inputted String, needle -> substring
         //return the position of substring appeared
+        let stringlength = self.operand.characters.count
+        var stringarray = Array(self.operand.characters)
+        let substringlength = needles.characters.count
+        var substringarray = Array(needles.characters)
+        
+        if stringlength < substringlength{
+            return -1
+        }
+        if stringlength <= 0 || substringlength <= 0{
+            return -1
+        }
+        
+        for i in 0..<stringlength {
+            if stringarray[i] == substringarray[0]{
+                var t = i
+                for j in 0..<substringlength{
+                    if stringarray[t] == substringarray[j]{
+                        t += 1
+                    }
+                    if t == substringlength + 1 {
+                        return i
+                    }
+                    
+                }
+            }
+            
+        }
         return -1
     }
 }
