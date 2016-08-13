69028f33a8b3fd0b486694a4229738a0a20c1b56
khlee12 <khlee1290@gmail.com>
2016-08-13 09:53:35 +0900

    Update ExprCore.swift

--- ExprEmpty/ExprCore.swift
+++ ExprEmpty/ExprCore.swift
@@ -10,6 +10,7 @@ import Foundation
 class ExprCore{
     
     private var operand:String=""
+    
     //operand stores the Strings that user inputted
     //numbers on the top, Strings on the buttom
     
@@ -20,95 +21,72 @@ class ExprCore{
     func doIsPalindrome()->String{
         //check whether the operand is palindrome
         //if yes -> return "Yes", otherwise return "NO"
-        if self.operand.hasPrefix("+") || self.operand.hasPrefix("-")
-        {
-            return "NO"
+        
+        // the length of the input
+        let n = operand.characters.count
+        
+        if 1 == n {
+            return "Yes"
         }
-        let compere: Bool? = (self.operand == doReverse())
-        if compere! == true {
-            return "YES"
+        
+        // convert the input to array
+        let array = Array(self.operand.characters)
+        
+        // compare the first and the last character, then the second..
+        for i in 0...n / 2 {
+            if array[i] == array[n - i - 1] {
+                continue
+            } else {
+                return "No"
+            }
         }
-        return "NO"
+        return "Yes"
     }
     
     func doReverse()->String{
         //reverse the operand(except the symbol) and return reversed string
         //let reversed = Array(self.operand.characters)[0]
-        let length = self.operand.characters.count
-        var reversed = Array(self.operand.characters)
-        var last = length - 1
-        var temp:Character
-        var start = 0
         
-        if self.operand.hasPrefix("+") || self.operand.hasPrefix("-"){
-            start = 1
-        }
-        for i in start..<length {
-            if i < last{
-                temp = reversed[i]
-                reversed[i] = reversed[last]
-                reversed[last] = temp
-                last -= 1
-            }
+        // convert the input to array
+        let array = Array(self.operand.characters)
+        let n = operand.characters.count
+        var m: Int = 0
+        var res: String = ""
+        
+        if "+" == array[0] || "-" == array[0] {
+            res.append(array[0])
+            m += 1
         }
         
-        var result:String = ""
-        
-        for i in 0..<length {
-            result.append(reversed[i])
+        for var i=n-1; i>=m; i-=1 {
+            res.append(array[i])
         }
-        return result
+        return res
     }
     func doStrStr(needles:String)->Int{
         //operand -> inputted String, needle -> substring
         //return the position of substring appeared
-        let stringlength = self.operand.characters.count
-        var stringarray = Array(self.operand.characters)
-        let substringlength = needles.characters.count
-        var substringarray = Array(needles.characters)
         
-        if stringlength < substringlength{
+        // convert the input to array
+        let array = Array(self.operand.characters)
+        let subArray = Array(needles.characters)
+        let n = self.operand.characters.count
+        let m = needles.characters.count
+        
+        if m > n {
             return -1
         }
-        if stringlength <= 0 || substringlength <= 0{
-            return -1
-        }
         
-        for i in 0..<stringlength {
-            if stringarray[i] == substringarray[0]{
-                var t = i
-                for j in 0..<substringlength{
-                    if stringarray[t] == substringarray[j]{
-                        t += 1
-                    }
-                    if t == substringlength + 1 {
-                        return i
-                    }
-                    
+        for var i in 0..<n {
+            var j: Int = i
+            var k: Int = 0
+            while array[j++] == subArray[k++] {
+                if k == m {
+                    return i
                 }
             }
-            
+            i = j
         }
         return -1
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
